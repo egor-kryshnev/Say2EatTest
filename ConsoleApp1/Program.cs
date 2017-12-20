b23dac825473310c4a4ff264c434840f9697e1e4
yochai <yochai@2cfe6abf-adfd-5d71-8db8-ce57b7db7bff>
2017-12-20 15:06:08 +0000

    test
    git-svn-id: https://github.com/yochail/Say2EatTest.git/trunk@1 2cfe6abf-adfd-5d71-8db8-ce57b7db7bff

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace say2EatTests
+{
+    public class Program
+    {
+        private static readonly string[] maindish = new string[] { "pizza", "toast" };
+        private static string[] addons = new string[] { "black olives", "green olives", "extra cheese", "mushrooms" };
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("insert your order");
+            string input = Console.ReadLine();
+            Console.WriteLine(AnalizeInput(input));
+             input = Console.ReadLine();
+        }
+
+        public static string AnalizeInput(string input)
+        {
+            var resualt = "";
+            if (string.IsNullOrEmpty(input))
+            {
+                Order order = new Order();
+                
+                foreach (var item in maindish)
+                {
+                    if (input.Contains(item))
+                    {
+                        order.main = item;
+
+                    }
+                }
+
+                foreach (var item in addons)
+                {
+                    if (input.Contains(item))
+                    {
+                        order.addons += item + ",";
+                    }
+                }
+                resualt =  "Your " + order.main + " with " + order.addons + "is on the way";
+            }
+            else
+            {
+                return "Please try again";
+            }
+
+            return resualt;
+        }
+
+        public class Order
+        {
+            public string addons;
+            public string main;
+        }
+    }
+}
+
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(say2EatTests.Program.AnalizeInput("i want to order a pizza with black olives"), "Your pizza with black olives,is on the way");
+        }
+    }
+}
