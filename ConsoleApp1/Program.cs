2bd03da0b2fb513e66a9b64c0fe922c9dad1af78
egor-kryshnev <32986555+egor-kryshnev@users.noreply.github.com>
2017-12-23 12:55:36 +0200

    YagorKryshniov

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,6 +4,11 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/**
+ I suggest to add the ability to order a few dishes at a time, with different additives.
+ I myself could write code for this, if necessary.
+     */
+
 namespace say2EatTests
 {
     public class Program
@@ -13,16 +18,23 @@ namespace say2EatTests
 
         static void Main(string[] args)
         {
-            Console.WriteLine("insert your order");
-            string input = Console.ReadLine();
-            Console.WriteLine(AnalizeInput(input));
-             input = Console.ReadLine();
+            string input;
+            do
+            {
+                Console.WriteLine("insert your order");       //I made a loop so that after the output "Please try again" the method was called again
+                input = Console.ReadLine();
+                Console.WriteLine(AnalizeInput(input));
+            } while (AnalizeInput(input).Equals("Please try again"));
+            input = Console.ReadLine();
         }
 
         public static string AnalizeInput(string input)
         {
+           
+            
             var resualt = "";
-            if (string.IsNullOrEmpty(input))
+            var tryAgain = "Please try again";
+            if (!string.IsNullOrEmpty(input))  //here was a bug
             {
                 Order order = new Order();
                 
@@ -33,20 +45,32 @@ namespace say2EatTests
                         order.main = item;
 
                     }
+                    
+                }
+                if (string.IsNullOrEmpty(order.main))          //I created a condition in which if the customer does not specify a dish, it returns "Please try again"
+                {    
+                    return tryAgain;
                 }
 
-                foreach (var item in addons)
+                foreach (var item in addons) 
                 {
                     if (input.Contains(item))
                     {
                         order.addons += item + ",";
                     }
                 }
-                resualt =  "Your " + order.main + " with " + order.addons + "is on the way";
+                if (string.IsNullOrEmpty(order.addons))                             //I created a condition in which if a customer orders a dish without an addons, displays "Your "dish" is on the way"
+                {
+                    resualt = "Your " + order.main + " is on the way";
+                }
+                else
+                {
+                    resualt = "Your " + order.main + " with " + order.addons + "is on the way";
+                }
             }
             else
             {
-                return "Please try again";
+                return tryAgain;
             }
 
             return resualt;
--- UnitTestProject1/UnitTest1.cs
+++ UnitTestProject1/UnitTest1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using say2EatTests;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace UnitTestProject1
@@ -7,9 +8,184 @@ namespace UnitTestProject1
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void AnalyzeInput_IfClientWantToOrderPizzaWithOneAddon()
         {
-            Assert.AreEqual(say2EatTests.Program.AnalizeInput("i want to order a pizza with black olives"), "Your pizza with black olives,is on the way");
+            //arrange
+            string input = "i want to order a pizza with black olives";
+            string expected = "Your pizza with black olives,is on the way";
+
+            //act            
+            string actual = Program.AnalizeInput(input); 
+
+            //assert
+            Assert.AreEqual(actual, expected);
         }
+
+        [TestMethod]
+        public void AnalyzeInput_IfClientWantToOrderToastWithOneAddon()
+        {
+            //arrange
+            string input = "i want to order a toast with black olives";
+            string expected = "Your toast with black olives,is on the way";
+
+            //act            
+            string actual = Program.AnalizeInput(input);
+
+            //assert
+            Assert.AreEqual(actual, expected);
+        }
+
+        [TestMethod]
+        public void AnalyzeInput_IfStringEmpty() {
+            //arrange
+            string emptyLine = "";
+            string expected = "Please try again";
+
+            //act
+            string actual = Program.AnalizeInput(emptyLine);
+          
+            //assert
+            Assert.AreEqual(actual, expected);
+
+        }
+
+        [TestMethod]
+        public void AnalyzeInput_IfClientWantOnlyPizzaWithoutAddon() {
+            //arrange
+            string input = "i want a pizza";
+            string expected = "Your pizza is on the way";
+
+            //act
+            string actual = Program.AnalizeInput(input);
+
+            //assert
+            Assert.AreEqual(actual, expected);
+        }
+
+        [TestMethod]
+        public void AnalyzeInput_IfClientWantOnlyToastWithoutAddon()
+        {
+            //arrange
+            string input = "i want a toast";
+            string expected = "Your toast is on the way";
+
+            //act
+            string actual = Program.AnalizeInput(input);
+
+            //assert
+            Assert.AreEqual(actual, expected);
+        }
+
+        [TestMethod]
+        public void AnalyzeInput_IfStringDoesNotHaveMaindish()
+        {
+            //arrange
+            string input = "BKSCBKJBHHSc xjccvxjkc jcnzjk mushrooms";
+            string expected = "Please try again";
+
+            //act
+            string actual = Program.AnalizeInput(input);
+
+            //assert
+            Assert.AreEqual(actual, expected);
+        }
+
+        [TestMethod]
+        public void AnalyzeInput_MethodReturnIsNotNull() {
+            //arrange
+            string maindish = "Pizza";
+           
+            //act
+            string value = Program.AnalizeInput(maindish);
+
+            //assert
+            Assert.IsNotNull(value);
+        }
+
+        [TestMethod]
+        public void AnalyzeInput_IfClientWantToastWith2Addons() {
+            //arrange
+            string input = "I want a toast with extra cheese, mushrooms";
+            string expected = "Your toast with extra cheese,mushrooms,is on the way";
+
+            //act
+            string actual = Program.AnalizeInput(input);
+
+            //assert
+            Assert.AreEqual(actual, expected);        
+        }
+
+        [TestMethod]
+        public void AnalyzeInput_IfClientWantPizzatWith2Addons()
+        {
+            //arrange
+            string input = "I want a pizza with extra cheese, mushrooms";
+            string expected = "Your pizza with extra cheese,mushrooms,is on the way";
+
+            //act
+            string actual = Program.AnalizeInput(input);
+
+            //assert
+            Assert.AreEqual(actual, expected);
+        }
+
+
+        [TestMethod]
+        public void AnalyzeInput_IfClientWantToastWith3Addons(){
+            //arrange
+            string input = "I want a toast with extra cheese, mushrooms, green olives";
+            string expected = "Your toast with green olives,extra cheese,mushrooms,is on the way";
+
+            //act
+            string actual = Program.AnalizeInput(input);
+
+            //assert
+            Assert.AreEqual(actual, expected);
+        }
+
+        [TestMethod]
+        public void AnalyzeInput_IfClientWantPizzaWith3Addons()
+        {
+            //arrange
+            string input = "I want a pizza with extra cheese, mushrooms, green olives";
+            string expected = "Your pizza with green olives,extra cheese,mushrooms,is on the way";
+
+            //act
+            string actual = Program.AnalizeInput(input);
+
+            //assert
+            Assert.AreEqual(actual, expected);
+        }
+
+        [TestMethod]
+        public void AnalyzeInput_IfClientWantPizzaWith4Addons()
+        {
+            //arrange
+            string input = "I want a pizza with extra cheese, mushrooms, green olives, black olives";
+            string expected = "Your pizza with black olives,green olives,extra cheese,mushrooms,is on the way";
+
+            //act
+            string actual = Program.AnalizeInput(input);
+
+            //assert
+            Assert.AreEqual(actual, expected);
+        }
+
+        [TestMethod]
+        public void AnalyzeInput_IfClientWantToastWith4Addons()
+        {
+            //arrange
+            string input = "I want a toast with extra cheese, mushrooms, green olives, black olives";
+            string expected = "Your toast with black olives,green olives,extra cheese,mushrooms,is on the way";
+
+            //act
+            string actual = Program.AnalizeInput(input);
+
+            //assert
+            Assert.AreEqual(actual, expected);
+        }      
+
+        
     }
+
 }
